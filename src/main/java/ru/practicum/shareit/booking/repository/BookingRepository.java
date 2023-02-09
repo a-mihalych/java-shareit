c908e6a094561da42b0c1cfc83a32ed0a277419b
a-mihalych <a-mihalych@yandex.ru>
2023-02-10 00:59:29 +0400

    feat: добавить бронирование вещей

--- src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
@@ -13,35 +13,46 @@ public interface BookingRepository extends JpaRepository<Booking, Integer> {
     List<Booking> findAllByBookerIdAndStartBeforeAndEndAfterOrderByStartDesc(Integer bookerId,
                                                                              LocalDateTime nowStart,
                                                                              LocalDateTime nowEnd);
+
     @Query("SELECT booking from Booking booking " +
             "where booking.item.owner.id = ?1 " +
             "and booking.start < ?2 " +
             "and booking.end > ?2 " +
             "order by booking.start desc")
     List<Booking> findAllCurrentForItemsOwner(Integer userId, LocalDateTime now);
+
     List<Booking> findAllByBookerIdAndEndBeforeOrderByStartDesc(Integer bookerId, LocalDateTime now);
+
     @Query("SELECT booking from Booking booking " +
             "where booking.item.owner.id = ?1 " +
             "and booking.end < ?2 " +
             "order by booking.start desc")
     List<Booking> findAllPastForItemsOwner(Integer userId, LocalDateTime now);
+
     List<Booking> findAllByBookerIdAndStartAfterOrderByStartDesc(Integer bookerId, LocalDateTime now);
+
     @Query("SELECT booking from Booking booking " +
             "where booking.item.owner.id = ?1 " +
             "and booking.start > ?2 " +
             "order by booking.start desc")
     List<Booking> findAllFutureForItemsOwner(Integer userId, LocalDateTime now);
+
     List<Booking> findAllByBookerIdAndStatusOrderByStartDesc(Integer bookerId, StatusBooking status);
+
     @Query("SELECT booking from Booking booking " +
             "where booking.item.owner.id = ?1 " +
             "and booking.status = ?2" +
             " order by booking.start desc")
     List<Booking> findAllForItemsOwnerByStatus(Integer userId, StatusBooking status);
+
     List<Booking> findAllByBookerIdOrderByStartDesc(Integer bookerId);
+
     @Query("SELECT booking from Booking booking " +
            "where booking.item.owner.id = ?1 " +
             "order by booking.start desc")
     List<Booking> findAllForItemsOwner(Integer userId);
+
     List<Booking> findAllByItemIdAndItemOwnerIdOrderByStartDesc(Integer itemId, Integer userId);
+
     List<Booking> findAllByBookerIdAndItemIdAndStartBefore(Integer userId, Integer itemId, LocalDateTime now);
 }
